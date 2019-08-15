744b21fede70c65dceba7175e0108dfd82cb24ed
Tiago Machado <tiagodjm@gmail.com>
2019-08-15 17:48:03 +1000

    Add votes, answers and views counter to question item

--- app/Question.php
+++ app/Question.php
@@ -23,4 +23,15 @@ class Question extends Model
 	public function getCreatedDateAttribute(){
    		return $this->created_at->diffForHumans(); // show date in days ago
    }
+
+   public function getStatusAttribute(){
+   		if ($this->answers > 0) {
+   			if ($this->best_answer_id) {
+   				return "answered-accepted";
+   			}
+   			return "answered";
+   		}
+   		return "unanswered";
+   	}	
 }
+
